8885b957fdb177e5d9035bb31f3f6f835564ba71
Anastasya Piskunova <nastya2000ru@mail.ru>
2020-05-23 23:18:20 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.mathKernel1 = new Wolfram.NETLink.MathKernel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(22, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(522, 524);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(579, 38);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(516, 524);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 633);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(303, 623);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 50);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(542, 622);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(187, 48);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "График";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(778, 622);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(186, 48);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Вывод";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // mathKernel1
+            // 
+            this.mathKernel1.AutoCloseLink = true;
+            this.mathKernel1.CaptureGraphics = true;
+            this.mathKernel1.CaptureMessages = true;
+            this.mathKernel1.CapturePrint = true;
+            this.mathKernel1.GraphicsFormat = "Automatic";
+            this.mathKernel1.GraphicsHeight = 0;
+            this.mathKernel1.GraphicsResolution = 0;
+            this.mathKernel1.GraphicsWidth = 0;
+            this.mathKernel1.HandleEvents = true;
+            this.mathKernel1.Input = null;
+            this.mathKernel1.Link = null;
+            this.mathKernel1.LinkArguments = null;
+            this.mathKernel1.PageWidth = 0;
+            this.mathKernel1.ResultFormat = Wolfram.NETLink.MathKernel.ResultFormatType.OutputForm;
+            this.mathKernel1.UseFrontEnd = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(30, 689);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(181, 26);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(30, 744);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(181, 26);
+            this.textBox3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1128, 865);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private Wolfram.NETLink.MathKernel mathKernel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Wolfram.NETLink;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MathKernel k = mathKernel1;
+            k.Compute("f[x_] := -0.75 x^2 - 6*x + 7.5;");
+            k.Compute("xm = f[4]*Sqrt[121]");
+            textBox1.Text = k.Result.ToString();
+
+            string c = k.Result.ToString();
+            char[] chars = c.ToCharArray();
+            for (int i = 0; i < c.Length; i++)
+                if (chars[i] == '.')
+                    chars[i] = ',';
+            c = new string(chars);
+            double x = Convert.ToDouble(c);
+            k.Compute("aa = Det[{{x,x+1},{x-1,x}}]");
+            textBox2.Text = k.Result.ToString();
+            if (x < 0)
+                MessageBox.Show("x<0");
+            else
+            {
+                k.Compute("t = Sqrt[xm]*Sin[xm]//N");
+                textBox3.Text = k.Result.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MathKernel k = mathKernel1;
+            k.GraphicsHeight = pictureBox1.Height;
+            k.GraphicsWidth = pictureBox1.Width;
+            k.CaptureGraphics = true;
+            k.GraphicsFormat = "JPEG";
+            k.Compute("Show[Plot[-0.75 x^2 - 6*x + 7.5,{x,-3,3}]]");
+            pictureBox1.Image = k.Graphics[0];
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = Image.FromFile("test.jpg");
+        }
+    }
+}
